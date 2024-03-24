21edd59e0867b5ed8861e68b0eb5c13df91f16f0
YehyunLee <glad7cu@gmail.com>
2024-03-24 13:39:29 -0400

    s0, s1

--- init_grid.asm
+++ init_grid.asm
@@ -26,8 +26,8 @@ init_grid:
     li $t2, 0xC0C0C0        # $t2 = brightgrey
     
     # INIT BASE ADDRESS
-    lw $t0, ADDR_DSPL       # $t0 = base address for display
-    li $t9, 0               # [For use of wall] counter to sub from t9
+    lw $s0, ADDR_DSPL       # $s0 = base address for display
+    li $s1, 0               # [For use of wall] counter to sub from t9
     
     # INIT LOOP COUNTER and PIXEL COLOUR
     li $t3, 0               # Loop COUNTER
@@ -35,35 +35,35 @@ init_grid:
 paint_loop:
     # Choose colour based on Pixel COLOUR flag
     beq $t4, 0, paint_dark_grey   # If flag is 0, call function for painting dark grey
-    sw $t2, 0($t0)          # If not, paint BRIGHT grey
+    sw $t2, 0($s0)          # If not, paint BRIGHT grey
     addi $t3, $t3, 1        # Increment loop counter: t3 += 1
-    addi $t0, $t0, 4        # Move to next pixel
-    addi $t9, $t9, 4        # [For later use]
-    sw $t2, 0($t0)          # If not, paint BRIGHT grey
+    addi $s0, $s0, 4        # Move to next pixel
+    addi $s1, $s1, 4        # [For later use]
+    sw $t2, 0($s0)          # If not, paint BRIGHT grey
     
     li $t4, 0               # Next time, paint DARK grey
     j paint_loop_end        # b or j is used to jump to different branch
                             # Here this continues to the next pixel
 
     # OLD CODE that I want to keep here:
-    # lw $t0, ADDR_DSPL       # $t0 = base address for display
-    # sw $t1, 0($t0)          # paint the first unit (i.e., top-left) red
-    # sw $t2, 4($t0)          # paint the second unit on the first row green
-    # sw $t3, 128($t0)        # paint the first unit on the second row blue
+    # lw $s0, ADDR_DSPL       # $s0 = base address for display
+    # sw $t1, 0($s0)          # paint the first unit (i.e., top-left) red
+    # sw $t2, 4($s0)          # paint the second unit on the first row green
+    # sw $t3, 128($s0)        # paint the first unit on the second row blue
 paint_dark_grey:
-    sw $t1, 0($t0)              # Paint dark grey
+    sw $t1, 0($s0)              # Paint dark grey
     addi $t3, $t3, 1            # Increment loop counter: t3 += 1
-    addi $t0, $t0, 4            # Move to next pixel
-    addi $t9, $t9, 4            # [For later use]
-    sw $t1, 0($t0)              # Paint dark grey
+    addi $s0, $s0, 4            # Move to next pixel
+    addi $s1, $s1, 4            # [For later use]
+    sw $t1, 0($s0)              # Paint dark grey
     
     li $t4, 1                 # Set colour FLAG to 1; meaning next time BRIGHT grey
     j paint_loop_end
 paint_loop_end:
     addi $t3, $t3, 1        # Increment loop counter: t3 += 1
-    addi $t0, $t0, 4        # Move to next pixel
+    addi $s0, $s0, 4        # Move to next pixel
                             # (by offset 4, since each pixel takes 4 bytes)
-    addi $t9, $t9, 4        # [For later use]
+    addi $s1, $s1, 4        # [For later use]
                             
     li $t5, 65536           # Load variable of 256 x 256
     
@@ -90,74 +90,74 @@ wall_initL:
     li $t8, 0x000000        # Black
     li $t1, 0               # Col counter
     li $t2, 0               # Row counter
-    sub $t0, $t0, $t9       # Subtract to get initial offset
-    li $t9, 0
+    sub $s0, $s0, $s1       # Subtract to get initial offset
+    li $s1, 0
     j left_wall
 reset_rowL:
     li $t2, 0
     addi $t1, $t1, 1
-    sub $t0, $t0, $t9       # Subtract to get initial offset
-    li $t9, 0
+    sub $s0, $s0, $s1       # Subtract to get initial offset
+    li $s1, 0
     mul $t5, $t1, 4              # 4*col
-    add $t0, $t0, $t5
-    add $t9, $t9, $t5
+    add $s0, $s0, $t5
+    add $s1, $s1, $t5
 left_wall:
     beq $t1, 6, wall_initR          # For loop
     beq $t2, 129, reset_rowL
-    sw $t8, 0($t0)
+    sw $t8, 0($s0)
     addi $t2, $t2, 1
-    addi $t0, $t0, 128
-    addi $t9, $t9, 128
+    addi $s0, $s0, 128
+    addi $s1, $s1, 128
     j left_wall
 wall_initR:
     # INIT
     li $t1, 104             # Col counter
     li $t2, 0               # Row counter
-    sub $t0, $t0, $t9       # Subtract to get initial offset
-    li $t9, 0
+    sub $s0, $s0, $s1       # Subtract to get initial offset
+    li $s1, 0
     
-    add $t0, $t0, $t1
-    add $t9, $t9, $t1
+    add $s0, $s0, $t1
+    add $s1, $s1, $t1
     j right_wall
 reset_rowR:
     li $t2, 0
     addi $t1, $t1, 4
-    sub $t0, $t0, $t9       # Subtract to get initial offset
-    li $t9, 0
+    sub $s0, $s0, $s1       # Subtract to get initial offset
+    li $s1, 0
     
-    add $t0, $t0, $t1
-    add $t9, $t9, $t1
+    add $s0, $s0, $t1
+    add $s1, $s1, $t1
 right_wall:
     beq $t1, 128, wall_initB          # For loop
     beq $t2, 129, reset_rowR
-    sw $t8, 0($t0)
+    sw $t8, 0($s0)
     addi $t2, $t2, 1
-    addi $t0, $t0, 128
-    addi $t9, $t9, 128
+    addi $s0, $s0, 128
+    addi $s1, $s1, 128
     j right_wall
 
 wall_initB:
     # INIT
     li $t1, 832               # Col counter
     li $t2, 0               # Row counter
-    sub $t0, $t0, $t9       # Subtract to get initial offset
-    li $t9, 0
+    sub $s0, $s0, $s1       # Subtract to get initial offset
+    li $s1, 0
     j bottom_wall
 reset_rowB:
     li $t2, 0
     addi $t1, $t1, 1
-    sub $t0, $t0, $t9       # Subtract to get initial offset
-    li $t9, 0
+    sub $s0, $s0, $s1       # Subtract to get initial offset
+    li $s1, 0
     mul $t5, $t1, 4              # 4*col
-    add $t0, $t0, $t5
-    add $t9, $t9, $t5
+    add $s0, $s0, $t5
+    add $s1, $s1, $t5
 bottom_wall:
     beq $t1, 864, exit          # For loop
     beq $t2, 6, reset_rowB
-    sw $t8, 0($t0)
+    sw $t8, 0($s0)
     addi $t2, $t2, 1
-    addi $t0, $t0, 128
-    addi $t9, $t9, 128
+    addi $s0, $s0, 128
+    addi $s1, $s1, 128
     j bottom_wall
 exit:
     li $v0, 10              # terminate the program gracefully
