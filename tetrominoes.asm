bf46949515fbcc52b96af4e9667f58be855f554e
aungzm06 <hisham.maw@mail.utoronto.ca>
2024-03-23 21:18:21 -0400

    Tetromino L and I is working
    Just O need some fixing
    Position is wrong

--- tetrominoes.asm
+++ tetrominoes.asm
@@ -25,8 +25,8 @@ ADDR_KBRD:
 ##############################################################################
 # Mutable Data
 ##############################################################################
-OTetrominoX: .word 1  # Sample X coordinate
-OTetrominoY: .word 2   # Sample Y coordinate
+OTetrominoX: .word 0  # Sample X coordinate
+OTetrominoY: .word 0   # Sample Y coordinate
 BlockColor: .word 0xff0000 #Block Color of tetrominoes for now
 BlockSize: .word 4  # 2 pixels by 2 bytes per pixel
 PIXEL: .word 2 # each pixel heigh and width
@@ -53,7 +53,7 @@ main:
     li $t3, 0x0000ff        # $t3 = blue
 
     lw $t0, ADDR_DSPL       # $t0 = base address for display
-	jal draw_tetromino_O
+	jal draw_tetromino_L
 
     # End or loop the game
     # For testing, we might not loop
@@ -62,72 +62,75 @@ main:
     syscall
     
 draw_tetromino_O:
-    lw $a2, ADDR_DSPL        # Load the base address of the display into $a2
-    lw $t4, OTetrominoX      # Load the X-coordinate
-    lw $t5, OTetrominoY      # Load the Y-coordinate
-
-    # Draw the first pixel at the top-left corner
-    li $a0, 1                # X offset from $t4
-    li $a1, 0                # Y offset from $t5
-    add $a0, $a0, $t4        # Calculate actual X-coordinate
-    add $a1, $a1, $t5        # Calculate actual Y-coordinate
-    jal draw_pixel           # Draw the pixel
-
-    # Draw the second pixel to the right of the first one
-    li $a0, 3                # X offset from $t4 for the second pixel
-    li $a1, 0                # Y offset from $t5 remains the same
-    add $a0, $a0, $t4        # Calculate actual X-coordinate for the second pixel
-    add $a1, $a1, $t5        # Calculate actual Y-coordinate remains the same
-    jal draw_pixel           # Draw the second pixel
+    lw $t0, ADDR_DSPL       # Load the base address of the display into $t0
+    lw $t4, OTetrominoX     # Load the X-coordinate
+    lw $t5, OTetrominoY     # Load the Y-coordinate
+    lw $t6, BlockColor      # Load the block color
     
-    # Ensure to add more calls to draw_pixel here for the rest of the "O" Tetromino pixels
-    
+    li $t8, 0                # Initialize Y loop counter (0 to 3)
+    y_loop:
+        blt $t8, 4, continue_y   # If Y loop counter < 4, continue
+        j end_y_loop             # Else, jump to the end of Y loop
+    continue_y:
+        li $t7, 0                # Initialize X loop counter (0 to 3)
+    x_loop:
+        blt $t7, 4, continue_x   # If X loop counter < 4, continue
+        j end_x_loop             # Else, jump to the end of X loop
+    continue_x:
+        # Calculate the offset $t7 = 4 * (x + 32 * y)
+        li $t1, 32               # Load 32 into $t1 (width of the display in pixels)
+        mul $t2, $t8, $t1        # $t2 = y * 32
+        add $t2, $t2, $t7        # $t2 = x + y * 32
+        li $t3, 4                # Load 4 into $t3 (bytes per pixel)
+        mul $t9, $t2, $t3        # $t9 = 4 * (x + 32 * y), the final offset in bytes
+        add $t9, $t0, $t9        # Add the base address of the display to $t9 to get the final memory address
+        
+        # Store the block color at the calculated address
+        sw $t6, 0($t9)           # Store the block color at the calculated address
+        
+        addi $t7, $t7, 1         # Increment X loop counter
+        j x_loop                 # Jump back to the start of the X loop
+    end_x_loop:
+        addi $t8, $t8, 1         # Increment Y loop counter
+        j y_loop                 # Jump back to the start of the Y loop
+    end_y_loop:
     jr $ra                   # Return from subroutine
-
-    
-    
 
 
     
 draw_tetromino_I:
-    lw $a2, ADDR_DSPL        # Load the base address of the display into $a2
-
-    # Load the top-left corner X and Y coordinates
-    lw $t4, OTetrominoX      # Load the X-coordinate into $t4
-    lw $t5, OTetrominoY      # Load the Y-coordinate into $t5
-
-    # Initialize Y-coordinate (row)
-    move $t1, $t5            # Start with the top-most row
-
-    # Loop for each of the 8 rows (height)
-    li $t2, 8                # Counter for rows
-    row_loop_I:
-        bgez $t2, end_row_loop_I   # Break the loop if counter is less than 0
+    lw $t0, ADDR_DSPL       # Load the base address of the display into $t0
+    lw $t4, OTetrominoX     # Load the X-coordinate
+    lw $t5, OTetrominoY     # Load the Y-coordinate
+    lw $t6, BlockColor      # Load the block color
     
-        # Initialize X-coordinate (column)
-        move $t0, $t4            # Start with the left-most column
-    
-        # Loop for each of the 2 columns (width)
-        li $t3, 2                # Counter for columns
-    column_loop_I:
-        bgez $t3, end_column_loop_I   # Break the loop if counter is less than 0
-    
-        # Set the coordinates for draw_pixel
-        move $a0, $t0            # Set X-coordinate
-        move $a1, $t1            # Set Y-coordinate
-    
-        jal draw_pixel           # Call subroutine to draw the pixel at (x, y)
-    
-        addi $t0, $t0, 1         # Move to the next column
-        addi $t3, $t3, -1        # Decrement column counter
-        j column_loop_I            # Repeat the loop for the next column
-    
-    end_column_loop_I:
-        addi $t1, $t1, 1         # Move to the next row
-        addi $t2, $t2, -1        # Decrement row counter
-        j row_loop_I               # Repeat the loop for the next row
-    
-    end_row_loop_I:
+    li $t8, 0                # Initialize Y loop counter (0 to 8)
+    y_loop:
+        blt $t8, 8, continue_y   # If Y loop counter < 8, continue
+        j end_y_loop             # Else, jump to the end of Y loop
+    continue_y:
+        li $t7, 0                # Initialize X loop counter (0 to <8)
+    x_loop:
+        blt $t7, 2, continue_x   # If X loop counter < 2, continue
+        j end_x_loop             # Else, jump to the end of X loop
+    continue_x:
+        # Calculate the offset $t7 = 4 * (x + 32 * y)
+        li $t1, 32               # Load 32 into $t1 (width of the display in pixels)
+        mul $t2, $t8, $t1        # $t2 = y * 32
+        add $t2, $t2, $t7        # $t2 = x + y * 32
+        li $t3, 4                # Load 4 into $t3 (bytes per pixel)
+        mul $t9, $t2, $t3        # $t9 = 4 * (x + 32 * y), the final offset in bytes
+        add $t9, $t0, $t9        # Add the base address of the display to $t9 to get the final memory address
+        
+        # Store the block color at the calculated address
+        sw $t6, 0($t9)           # Store the block color at the calculated address
+        
+        addi $t7, $t7, 1         # Increment X loop counter
+        j x_loop                 # Jump back to the start of the X loop
+    end_x_loop:
+        addi $t8, $t8, 1         # Increment Y loop counter
+        j y_loop                 # Jump back to the start of the Y loop
+    end_y_loop:
         jr $ra                   # Return from subroutine
     
     
@@ -146,115 +149,65 @@ draw_tetromino_Z: #subroutine to draw square tetromino
     
 # Subroutine to draw the "L" Tetromino
 draw_tetromino_L:
-    lw $t1, ADDR_DSPL        # Load the base address of the display into $t1
-
-    # Load the top-left corner X and Y coordinates
-    lw $t4, OTetrominoX      # Load the X-coordinate into $t4
-    lw $t5, OTetrominoY      # Load the Y-coordinate into $t5
-
-    # Initialize Y-coordinate (row)
-    move $t1, $t5            # Start with the top-most row
-
-    # Loop for each of the 6 rows (height of the main body)
-    li $t2, 6                # Counter for rows
-    row_loop_L:
-        bgez $t2, foot_L         # When row counter is 0, proceed to draw the foot of the L
+        lw $t0, ADDR_DSPL       # Load the base address of the display into $t0
+        lw $t4, OTetrominoX     # Load the X-coordinate into $t4
+        lw $t5, OTetrominoY     # Load the Y-coordinate into $t5
+        lw $t6, BlockColor      # Load the block color into $t6
     
-        # Initialize X-coordinate (column)
-        move $t0, $t4            # Start with the left-most column
+        # Draw the main body of the L
+        li $t8, 0                # Y loop counter for the main body
+    main_body_loop:
+        blt $t8, 6, continue_main_body  # If Y loop counter < 7, continue
+        j draw_foot              # Else, proceed to draw the foot
+    continue_main_body:
+        li $t7, 0                # X loop counter for the main body
+    x_loop_main_body:
+        blt $t7, 2, continue_x_main_body  # If X loop counter < 2, continue
+        j end_x_loop_main_body   # Else, end the X loop
+    continue_x_main_body:
+        # Calculate the offset and draw
+        li $t1, 32               # Width of the display in pixels
+        mul $t2, $t8, $t1        # y * width
+        add $t2, $t2, $t7        # (y * width) + x
+        li $t3, 4                # Bytes per pixel
+        mul $t2, $t2, $t3        # Offset in bytes
+        add $t2, $t0, $t2        # Final address
+        sw $t6, 0($t2)           # Store the color
     
-        # Loop for each of the 2 columns (width of the main body)
-        li $t3, 2                # Counter for columns
-    column_loop_L:
-        bgez $t3, end_column_loop_L  # Break the loop if column counter is less than 0
+        addi $t7, $t7, 1         # Increment X loop counter
+        j x_loop_main_body       # Jump back to start of X loop
+    end_x_loop_main_body:
+        addi $t8, $t8, 1         # Increment Y loop counter
+        j main_body_loop         # Jump back to start of main body loop
     
-        # Set the coordinates for draw_pixel
-        move $a0, $t0            # Set X-coordinate
-        move $a1, $t1            # Set Y-coordinate
-        lw $a2, ADDR_DSPL        # Load the display address
+    draw_foot:
+        # Starting from row 4, for 2 additional pixels to create the foot of the L
+        li $t8, 4                # Y loop counter for the foot, start from row 4
+        li $t9, 2                # Two additional iterations for the foot
+    foot_loop:
+        li $t7, 0                # Start drawing foot from column 2
+        add $t7, $t7, $t9        # Calculate end column for foot drawing
+        blt $t9, 4, continue_foot  # Continue if within foot drawing range
+        j end_foot               # Else, end the foot loop
+    continue_foot:
+        # Calculate the offset and draw the foot
+        li $t1, 32               # Width of the display in pixels
+        mul $t2, $t8, $t1        # y * width
+        add $t2, $t2, $t7        # (y * width) + x
+        li $t3, 4                # Bytes per pixel
+        mul $t2, $t2, $t3        # Offset in bytes
+        add $t2, $t0, $t2        # Final address for the foot
+        sw $t6, 0($t2)           # Store the color for the foot
+        sw $t6, 128($t2)           # Store the color for the foot
     
-        jal draw_pixel           # Call subroutine to draw the pixel
-    
-        addi $t0, $t0, 1         # Move to the next column
-        addi $t3, $t3, -1        # Decrement column counter
-        j column_loop_L          # Repeat the loop for the next column
-    
-    end_column_loop_L:
-        addi $t1, $t1, 1         # Move to the next row
-        addi $t2, $t2, -1        # Decrement row counter
-        j row_loop_L             # Repeat the loop for the next row
-    
-    # After the main body is drawn, draw the foot of the "L"
-    foot_L:
-        # Adjust the x and y coordinates for the foot pixels
-        addi $a0, $t4, 2         # Column 3
-        addi $a1, $t5, 4         # Row 5
-        lw $a2, ADDR_DSPL
-        jal draw_pixel           # Draw pixel at (3,5)
-        
-        addi $a0, $t4, 3         # Column 4
-        jal draw_pixel           # Draw pixel at (4,5)
-    
-        addi $a1, $t5, 5         # Row 6
-        jal draw_pixel           # Draw pixel at (4,6)
-    
-        addi $a0, $t4, 2         # Column 3
-        jal draw_pixel           # Draw pixel at (3,6)
-    
+        addi $t9, $t9, 1         # Increment foot drawing counter
+        j foot_loop              # Jump back to start of foot loop
+    end_foot:
         jr $ra                   # Return from subroutine
 
-# Subroutine to draw the "J" Tetromino
+
 draw_tetromino_J:
-    lw $t1, ADDR_DSPL        # Load the base address of the display into $t1
-
-    # Load the top-left corner X and Y coordinates
-    lw $t4, OTetrominoX      # Load the X-coordinate into $t4
-    lw $t5, OTetrominoY      # Load the Y-coordinate into $t5
-
-    # Initialize Y-coordinate (row)
-    move $t1, $t5            # Start with the top-most row
-
-    # Loop for each of the 6 rows (height of the main body)
-    li $t2, 6                # Counter for rows
-    row_loop_J:
-        bgez $t2, foot_J         # When row counter is 0, proceed to draw the foot of the J
     
-        # Initialize X-coordinate (column)
-        move $t0, $t4            # Start with the left-most column
-    
-        # Loop for each of the 2 columns (width of the main body)
-        li $t3, 2                # Counter for columns
-    column_loop_J:
-        bgez $t3, end_column_loop_J  # Break the loop if column counter is less than 0
-    
-        # Set the coordinates for draw_pixel
-        move $a0, $t0            # Set X-coordinate
-        move $a1, $t1            # Set Y-coordinate
-        lw $a2, ADDR_DSPL        # Load the display address
-    
-        jal draw_pixel           # Call subroutine to draw the pixel
-    
-        addi $t0, $t0, 1         # Move to the next column
-        addi $t3, $t3, -1        # Decrement column counter
-        j column_loop_J          # Repeat the loop for the next column
-    
-    end_column_loop_J:
-        addi $t1, $t1, 1         # Move to the next row
-        addi $t2, $t2, -1        # Decrement row counter
-        j row_loop_J             # Repeat the loop for the next row
-    
-    # After the main body is drawn, draw the foot of the "J"
-    foot_J:
-        # Adjust the x coordinate for the foot pixels to be one position to the left of the main body
-        addi $a0, $t4, -1        # Column 0, to the left of the main body start
-        addi $a1, $t5, 4         # Row 5
-        lw $a2, ADDR_DSPL
-        jal draw_pixel           # Draw pixel at (0,5)
-        
-        addi $a1, $t5, 5         # Row 6
-        jal draw_pixel           # Draw pixel at (0,6)
-    
-        jr $ra                   # Return from subroutine
 
 
 
@@ -265,25 +218,4 @@ draw_tetromino_T: #subroutine to draw square tetromino
     lw $a1, OTetrominoY # X-coordinate
     
     
-    .text
-    .globl draw_pixel
-
-
-draw_pixel:
-    lw $t0, ADDR_DSPL        # Base address of the display
-    li $t1, 32               # Assuming display width is 32 pixels (words) per row
-    
-    mul $t2, $a1, $t1        # Calculate row offset in memory (Y-coordinate * display width)
-    sll $t2, $t2, 2          # Convert row offset to bytes by shifting left by 2 (mul by 4)
-    
-    sll $a0, $a0, 2          # Convert X-coordinate to byte offset by shifting left by 2
-    add $t0, $t0, $t2        # Add row offset to base address
-    add $t0, $t0, $a0        # Add column offset to address
-
-    lw $t4, BlockColor       # Load the block color
-    sw $t4, 0($t0)           # Store the color at the calculated address
-
-    jr $ra                   # Return from subroutine
-
-
 
